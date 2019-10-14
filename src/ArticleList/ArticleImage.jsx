3d026c087c2b3e2b574b6faaa5c0432807633c9d
Renzi Meng <mengrenzi1996@gmail.com>
2019-10-14 14:37:13 -0700

    assignment 6

---
+++ src/ArticleList/ArticleImage.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import './ArticleImage.module.css';
+
+const ArticleImage = (props) => {
+  return (
+    <img src= {props.article.image._url} alt={props.article.title}/>
+  );
+}
+ArticleImage.propTypes = {
+    article: PropTypes.object.isRequired
+};
+export default ArticleImage;
--- src/ArticleList/ArticleListItem.jsx
+++ src/ArticleList/ArticleListItem.jsx
@@ -1,15 +1,21 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import SlugButton from './SlugButton.jsx'
+import ArticleImage from './ArticleImage'
 import './ArticleListItem.module.css'
 
 const ArticleListItem = props => (
-    <section>
-        <h2>{props.article.title}</h2>
-        <p>{props.article.shortText}</p>
-        <time dateTime = {props.article.pubYear}>{props.article.pubDate}</time>
-        {<SlugButton article = {props.article}/>}
-    </section>
+    <div>
+        <div>
+            {<ArticleImage article = {props.article}/>}
+        </div>
+        <section>
+            <h2>{props.article.title}</h2>
+            <p>{props.article.shortText}</p>
+            <time dateTime = {props.article.pubYear}>{props.article.pubDate}</time>
+            {<SlugButton article = {props.article}/>}
+        </section>
+    </div>
 );
 
 
